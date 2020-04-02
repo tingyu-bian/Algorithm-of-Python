ce53207f09f5222a2e07b29ad46aee1052f529c4
tingyu-bian <49895683+tingyu-bian@users.noreply.github.com>
2020-04-02 10:22:54 +0800

    Add files via upload

---
+++ Simple level algorithm problem/整数反转.py
@@ -0,0 +1,36 @@
+
+# 问题：
+# 给出一个 32 位的有符号整数，你需要将这个整数中每位上的数字进行反转。
+#
+# 示例 1:
+# 输入: 123
+# 输出: 321
+#  
+# 示例 2:
+# 输入: -123
+# 输出: -321
+#
+# 示例 3:
+# 输入: 120
+# 输出: 21
+
+# 注意:
+# 假设我们的环境只能存储得下 32 位的有符号整数，则其数值范围为 [−231,  231 − 1]。
+# 请根据这个假设，如果反转后整数溢出那么就返回 0。
+
+
+class IntegerReverse(object):
+    @staticmethod
+    def reverse(int_num):
+        str_num = str(int_num)[::-1]
+        if str_num.endswith('-'):
+            str_num = '-' + str_num[:-1]
+            return int(str_num) if int(str_num) >= -2 ** 31 else 0
+        return int(str_num) if int(str_num) <= 2 ** 31 - 1 else 0
+
+
+
+
+
+
+
